8d00491f141298732887df2b87c36a569bc165de
Edward Ridge <edward@abaresearch.co.uk>
2015-08-25 16:46:11 +0100

    Added gulp task to process SASS
    Added package.json, gulpfile.js and Site.scss

---
+++ Gulp and Visual Studio/gulpfile.js
@@ -0,0 +1,20 @@
+﻿var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('sass', function () {
+    gulp.src('./Content/*.scss')
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass())
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.sourcemaps.write())
+        .pipe(gulp.dest('./Content'))
+        .pipe(plugins.livereload())
+    ;
+});
+
+gulp.task('watch-sass', function () {
+    plugins.livereload.listen();
+    gulp.watch('./Content/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch-sass']);
